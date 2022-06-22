3a5d0240b833253d91f81c8794864dee6fc61afd
Yohan Centanni <thorbenliya@gmail.com>
2022-06-22 16:04:47 +0200

    Add dashboard view template, scss and public folder

---
+++ www/View/dashboard.view.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Design guide</title>
+		<link rel="stylesheet" type="text/css" href="dist/main.css">
+		<script src="https://kit.fontawesome.com/f3633012bb.js" crossorigin="anonymous"></script>
+	</head>
+	<script type="text/javascript" src="dist/main.js"></script>
+	<body>
+		<header>
+			<div class="container">
+				<span class="title_navbar">Nom du site</span>
+				<span class="profil_pic">
+					photo
+				</span>
+			</div>
+		</header>
+		<div class="sidebar">
+				 	<ul>
+					<li><i class="fa-solid fa-house"></i> </li> 
+					 <li><i class="fa-solid fa-dollar-sign"></i></li> 
+					 <li> <i class="fa-solid fa-box"></i> </li>
+					<li> <i class="fa-solid fa-user"></i> </li>
+					<li> <a href="pages.html"><i class="fa-solid fa-file"></i> </a> </li>
+					<li> <i class="fa-solid fa-gear"></i> </li>
+					</ul>
+		</div>
+		<div class="container">
+		<h1 class="title">Dashboard</h1>
+			<section class="resumenumbers">
+				<article class="numbers"><p class="bignumbers">1000€</p><p class="smallnumbers">Chiffres d'affaires quotidien</p></article>
+				<article class="numbers"><p class="bignumbers">100€</p><p class="smallnumbers">Chiffres d'affaires quotidien</p></article>
+				<article class="numbers"><p class="bignumbers">1000€</p><p class="smallnumbers">Chiffres d'affaires quotidien</p></article>
+				<article class="numbers"><p class="bignumbers">1000€</p><p class="smallnumbers">Chiffres d'affaires quotidien</p><p class="smallestnumber"><i class="fa-solid fa-arrow-trend-up"></i> 15%</p></article>
+			</section>
+			<section class="graph-container">
+				<div >
+					<p class="graphtitle">Vente des 30 derniers jours</p>
+					 <div class="button button--dark select">
+						Echelle
+						<i class="fa-solid fa-angle-down"></i>
+						<ul style="display: none;">
+							<li>Pas visibilité</li>
+						</ul>
+					</div>
+						<img src="assets/images/Graphique.svg" width="100%">
+				</div>
+			</section>
+	
+
+		
+		</div>
+	</body>
+</html>
